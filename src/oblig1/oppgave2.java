8cb78c87585073a5f789866177f81cdbf40fca26
Kristoffer Berdal <web@flexd.net>
2012-02-23 12:57:37 +0100

    initial commit

---
+++ src/oblig1/oppgave2.java
@@ -0,0 +1,17 @@
+/*
+ */
+package oblig1;
+
+/**
+ *
+ * @author Kristoffer Berdal <web@flexd.net>
+ */
+public class oppgave2 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+}
